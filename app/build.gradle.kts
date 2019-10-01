aea0a2f1f79b355cc606baf5911b545757d2ab5a
Kelvin Du <kelvin.fto@gmail.com>
2019-10-01 17:15:42 +0700

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,38 @@
+import org.jetbrains.kotlin.config.KotlinCompilerVersion
+
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("android.extensions")
+}
+
+android {
+    compileSdkVersion(28)
+    defaultConfig {
+        applicationId = "com.example.moviemvi"
+        minSdkVersion(15)
+        targetSdkVersion(28)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation(kotlin("stdlib-jdk7", KotlinCompilerVersion.VERSION))
+    implementation("androidx.appcompat:appcompat:1.0.0-beta01")
+    implementation("androidx.core:core-ktx:1.2.0-alpha04")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0-beta01")
+    implementation("androidx.recyclerview:recyclerview:1.0.0-beta01")
+    implementation("com.google.android.material:material:1.0.0-beta01")
+    testImplementation("junit:junit:4.12")
+    androidTestImplementation("androidx.test:runner:1.1.0-alpha4")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.1.0-alpha4")
+}
